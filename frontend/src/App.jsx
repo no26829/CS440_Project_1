61d669a007531fabbcbcf020d75300caf6b6361d
no26829 <102497309+no26829@users.noreply.github.com>
2025-02-13 07:56:20 -0700

    Update and rename App.js to App.jsx

---
+++ frontend/src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import VirtualPet from './components/VirtualPet';
+
+// Main App component
+function App() {
+  return (
+    <div className="App">
+      <VirtualPet />
+    </div>
+  );
+}
+
+export default App;
